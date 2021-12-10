3d8ec9f56eea2de5115cda1249c482ac20e44dbe
Zyix09 <ccebol33@gmail.com>
2021-12-10 10:37:14 +0700

    first commit

---
+++ cetak.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html>
+     <head>
+    </head> 
+          <body>
+               <?php 
+               $txtl = "Praktikum PHP";
+               $txt2 = "Variabel, Tipe Data, String";
+                 echo "<h2> $txt1 </h2>";
+                 echo "Materi yang dipelajari " . $txt2; 
+
+                  print "<h2> $txtl </h2>";
+                  print "Materi yang dipelajari " . $txt2; 
+                  ?> 
+                  </body> 
+                  </html>
